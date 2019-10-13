69dd78ee42527a80fd827890704829dedfaa7b66
hlxsrc <alexisrodcal@gmail.com>
2019-10-13 17:16:09 -0500

    Add class OpenFile to open txt files

--- BoundarySmoothing/main.py
+++ BoundarySmoothing/main.py
@@ -1,30 +1,68 @@
 from tkinter import *
+from tkinter import filedialog
 
 
-# Setting root
-root = Tk()
+def read_file(filename):
 
-# Setting title
-root.title('Decision Boundary Smoothing')
+    f = open(filename, "r")
+    text = f.read()
+    return text
 
-# Setting menu in top bar
-menu = Menu(root)
-root.config(menu=menu)
-filemenu = Menu(menu)
-menu.add_cascade(label='File', menu=filemenu)
-filemenu.add_command(label='Open...')
-filemenu.add_separator()
-filemenu.add_command(label='Exit', command=root.quit)
-helpmenu = Menu(menu)
-menu.add_cascade(label='Help', menu=helpmenu)
-helpmenu.add_command(label='About')
 
-# Setting buttons
-buttonGraphic = Button(root, text='Show graphic', width=25, command=root.destroy)
-buttonSmoothing = Button(root, text='Smoothing', width=25, command=root.destroy)
-buttonGraphic.pack()
-buttonSmoothing.pack();
+class OpenFile(Frame):
 
-# Main
-mainloop()
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
 
+        self.parent = parent
+        self.init_ui()
+
+    def init_ui(self):
+        self.pack(fill=BOTH, expand=1)
+
+        # Setting menu in top bar
+        menu_bar = Menu(self.parent)
+        self.parent.config(menu=menu_bar)
+
+        # Setting File menu
+        file_menu = Menu(menu_bar)
+        menu_bar.add_cascade(label='File', menu=file_menu)
+        file_menu.add_command(label='Open...', command=self.on_open)
+        file_menu.add_separator()
+        file_menu.add_command(label='Exit', command=self.parent.quit)
+
+        # Setting Help menu
+        help_menu = Menu(menu_bar)
+        menu_bar.add_cascade(label='Help', menu=help_menu)
+        help_menu.add_command(label='About')
+
+        # Setting buttons
+        button_graphic = Button(self.parent, text='Show graphic', width=25, command=self.parent.destroy)
+        button_smoothing = Button(self.parent, text='Smoothing', width=25, command=self.parent.destroy)
+        button_graphic.pack()
+        button_smoothing.pack();
+
+    def on_open(self):
+
+        ftypes = [('Text files', '*.txt'), ('All files', '*')]
+        dlg = filedialog.Open(self, filetypes=ftypes)
+        fl = dlg.show()
+
+        if fl != '':
+            text = read_file(fl)
+
+
+def main():
+
+    # Setting root
+    root = Tk()
+
+    # Setting title
+    root.title('Decision Boundary Smoothing')
+
+    of = OpenFile(root)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
