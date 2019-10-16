6a3dfa7b542fc5498a7696a62194437db07c176f
hlxsrc <alexisrodcal@gmail.com>
2019-10-16 17:44:22 -0500

    Change on_open to plot attributes according to classes

--- BoundarySmoothing/main.py
+++ BoundarySmoothing/main.py
@@ -1,23 +1,40 @@
 from tkinter import *
 from tkinter import filedialog
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-def read_file(filename):
+def read_file(file_name):  # Function to read file
 
-    f = open(filename, "r")
-    text = f.read()
-    return text
+    f = open(file_name, "r")
+    lines = f.readlines()
+    return lines
 
 
-class OpenFile(Frame):
+def plot(x, y):  # Function to plot data
+    # plot the training loss and accuracy
+    plt.style.use("ggplot")
+    plt.figure()
+    plt.plot(x, y)
+
+    plt.title("Training Loss and Accuracy")
+    plt.xlabel("Epoch #")
+    plt.ylabel("Loss/Accuracy")
+    plt.legend()
+    plt.show()
+
+
+class OpenFile(Frame):  # Class to open txt file with GUI
 
     def __init__(self, parent):
+
         Frame.__init__(self, parent)
 
         self.parent = parent
         self.init_ui()
 
-    def init_ui(self):
+    def init_ui(self):  # Define GUI
+
         self.pack(fill=BOTH, expand=1)
 
         # Setting menu in top bar
@@ -40,7 +57,7 @@ class OpenFile(Frame):
         button_graphic = Button(self.parent, text='Show graphic', width=25, command=self.parent.destroy)
         button_smoothing = Button(self.parent, text='Smoothing', width=25, command=self.parent.destroy)
         button_graphic.pack()
-        button_smoothing.pack();
+        button_smoothing.pack()
 
     def on_open(self):
 
@@ -49,10 +66,44 @@ class OpenFile(Frame):
         fl = dlg.show()
 
         if fl != '':
-            text = read_file(fl)
+
+            # read the file
+            lines = read_file(fl)
+
+            # store configuration values
+            size = lines[0].strip()
+            attr = lines[1].strip()
+            classes = lines[2].strip()
+
+            A = np.loadtxt(fl, skiprows=3, delimiter=',')
+
+            # attributes to plot given by user
+            attr1 = 0
+            attr2 = 1
+
+            # attribute's array
+            list1 = []
+            list2 = []
+
+            plt.figure()
+
+            # takes values from matrix
+            for i in range(int(classes)):
+                for j in range(int(size)):
+                    if A[j][int(attr)] == i:
+                        list1.append(A[j][attr1])
+                        list2.append(A[j][attr2])
+                plt.scatter(list1, list2, marker='o')
+                list1.clear()
+                list2.clear()
+
+            plt.title("Comparison")
+            plt.xlabel("Attribute {}".format(attr1))
+            plt.ylabel("Attribute {}".format(attr2))
+            plt.show()
 
 
-def main():
+def main():  # Main Program
 
     # Setting root
     root = Tk()
@@ -60,6 +111,7 @@ def main():
     # Setting title
     root.title('Decision Boundary Smoothing')
 
+    # Creates a new instance
     of = OpenFile(root)
     root.mainloop()
 
